8c4d1939737a4bb31a9d75ddb34aff0fe7169048
Wyatt Cade <cadewya@tamu.edu>
2024-03-23 18:04:45 -0500

    Display menu items as cards

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
 
-export default function Home() {
+export const getMenuItems = async () => {
+  const items = await fetch("http://localhost:5000/api/menuitems")
+  const data = await items.json()
+
+  return data
+}
+
+export default async function Home() {
+  const menuItems = await getMenuItems()
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1>Hello World!</h1>
+    <main className="min-h-screen flex flex-column items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        
+      <h1>MENU ITEMS</h1>
+      {menuItems.map(item => (
+        <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+        <h5 key = {item.menuid} className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.itemname}</h5>
+        </a>
+      ))}
+      </div>
     </main>
   );
 }
