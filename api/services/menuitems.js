e5337391671788c7ca5686d11bce5bb89476e9fd
Isaac Ambro <isaacambro@gmail.com>
2024-03-24 19:49:17 -0500

    Added backend logic for adding menu items

--- api/services/menuitems.js
+++ api/services/menuitems.js
@@ -11,19 +11,37 @@ const retrieveMenuItems = (req,res) => {
 
 const addMenuItem = (req, res) => {
     const { itemName, price, category } = req.body;
-    db.query(
-        'INSERT INTO menuitems (itemName, price, category) VALUES ($1, $2, $3)',
-        [itemName, price, category],
-        (err, result) => {
-            if (err) {
-                throw err;
-            }
-            res.status(201).send('Menu item added successfully');
-        }
-    );
+	db.query(
+		"Select * from menuitems where itemName = $1", [itemName],
+		(err, result) => {
+			if (err){
+				console.log(err);
+				res.status(500);
+				return;
+			} else if (result.rows.length){
+				res.status(401).send("Item Already exists");
+				return;
+			} else {
+				db.query(
+					'INSERT INTO menuitems (itemName, price, category) VALUES ($1, $2, $3)',
+					[itemName, price, category],
+					(err, result) => {
+						if (err) {
+							console.log(err);
+							res.status(500);
+							return;
+						}
+						res.status(201).send('Menu item added successfully');
+					}
+				);
+			}
+		}
+	);
+    
 };
 
 module.exports = {
 	retrieveMenuItems,
 	addMenuItem,
+
 }
