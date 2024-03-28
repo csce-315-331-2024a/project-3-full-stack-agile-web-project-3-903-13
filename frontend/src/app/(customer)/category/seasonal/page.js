28a7568e0bcc0026f65047674219a3f8023bdd4f
sukelv0802 <sukelv0802@gmail.com>
2024-03-28 08:45:02 -0500

    Added routing to rest of the buttons

--- frontend/src/app/(customer)/category/seasonal/page.js
+++ frontend/src/app/(customer)/category/seasonal/page.js
@@ -1,15 +1,15 @@
 "use client";
 import { useEffect, useState } from 'react';
 
-const fries = () => {
-    const [fries, setfries] = useState([]);
+const seasonals = () => {
+    const [seasonals, setseasonals] = useState([]);
 
     useEffect(() => {
         const fetchMenuItems = async () => {
             const response = await fetch('http://localhost:5000/api/menuitems');
             const data = await response.json();
-            const frieItems = data.filter(item => item.category === 3);
-            setfries(frieItems);
+            const seasonalItems = data.filter(item => item.category === 6);
+            setseasonals(seasonalItems);
         };
 
         fetchMenuItems();
@@ -17,18 +17,18 @@ const fries = () => {
 
     return (
         <main className="min-h-screen bg-cream py-10">
-            <div className="container mx-auto">
-                <h1 className="text-3xl font-bold text-center mb-8">Fries</h1>
+            <div className="container mx-auto"> 
+                <h1 className="text-3xl font-bold text-center mb-8">Seasonals</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {fries.map((frie) => (
-                        <div key={frie.menuID} className="relative bg-white rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out aspect-square">
+                    {seasonals.map((seasonal) => (
+                        <div key={seasonal.menuID} className="relative bg-white rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out aspect-square">
                             <img
                                 src={"/* IDK HOW TO GET THIS TO WORK */"}
-                                alt={frie.itemname}
+                                alt={seasonal.itemname}
                                 className="object-cover w-full h-full rounded-lg"
                             />
                             <div className="absolute inset-0 flex flex-col justify-end p-4">
-                                <h5 className="text-xl font-bold text-gray-900 text-center">{frie.itemname}</h5>
+                                <h5 className="text-xl font-bold text-gray-900 text-center">{seasonal.itemname}</h5>
                             </div>
                         </div>
                     ))}
@@ -38,4 +38,4 @@ const fries = () => {
     );
 };
 
-export default fries;
+export default seasonals;
