616a5483cba2584ce97920b2ba5f0a926ee4c2b2
Wyatt Cade <cadewya@tamu.edu>
2024-03-27 19:20:46 -0500

    Retrieve results from database

--- api/services/reports/whatSellsTogether.js
+++ api/services/reports/whatSellsTogether.js
@@ -4,19 +4,32 @@ const generateReport = (req,res) => {
 	const startDate = req.body.startDate
 	const endDate = req.body.endDate
 
+	console.log(startDate)
+
 	// Report requires multiple queries. First, we need to make a view,
 	// then, we need to make a join.
 	const viewQuery = `CREATE VIEW valid AS (SELECT pairs.transactionid, pairs.item1, pairs.item2 FROM 
 		pairs JOIN transactions ON pairs.transactionid = transactions.transactionid AND transactions.transactiontime 
-		<= $2::timestamp AND transactions.transactiontime >= $1::timestamp)`
+		<= '${endDate}' AND transactions.transactiontime >= '${startDate}')`
 	
-	db.query(viewQuery, [startDate, endDate], (err, results) => {
-
+	db.query(viewQuery, (err, results) => {
 			if (err) {
 				res.status(500)
 				throw err;
 			} else {
-				res.status(200).json(results.rows.length)
+				const selectQuery = `SELECT m1name, m2name, count(*) AS paircount FROM (SELECT m1name, m2name FROM valid JOIN menupairs ON item1 = menupairs.m1id AND item2 = menupairs.m2id) AS nameditems GROUP BY m1name, m2name ORDER BY paircount DESC;`
+				db.query(selectQuery, (err, results) => {
+					if (err) {
+						res.status(500)
+						throw err
+					}
+					res.status(200).json(results.rows)
+					db.query("DROP VIEW valid", (err, results) => {
+						if (err) {
+							throw err
+						}
+					})
+				})
 			}
 	})
 }
