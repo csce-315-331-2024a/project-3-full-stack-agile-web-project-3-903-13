0b73377dc98d98d0fb355148d9770b2402da08bd
Wyatt Cade <cadewya@tamu.edu>
2024-03-29 22:01:20 -0500

    Insert new transaction into the database

--- api/services/transactions.js
+++ api/services/transactions.js
@@ -1,6 +1,26 @@
+const db = require('../config/db')
+
+const insertTransaction = (totalCost, taxAmount) => {
+	var currentDate = new Date().toLocaleString()
+	db.query("INSERT INTO transactions values (DEFAULT, $1::timestamp, $2, $3)", [currentDate,totalCost,taxAmount], (err,results) => {
+		if (err) {
+			throw err;
+		}
+	})
+}
+
 const createTransaction = (req,res) => {
-	const totalCost = req.totalCost
-	const taxAmount = req.taxAmount
+	const totalCost = req.body.totalCost
+	const taxAmount = req.body.taxAmount
+	const orderContents = req.body.orderContents
+
+	try {
+		insertTransaction(totalCost,taxAmount)
+	} catch (error) {
+		console.log(error)	
+	}
+
+	console.log("new tranaction yay!")
 }
 
 module.exports = {
