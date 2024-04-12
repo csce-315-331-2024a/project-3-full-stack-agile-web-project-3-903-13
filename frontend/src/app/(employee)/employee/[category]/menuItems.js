47c3d7683469f8fa052502503753086991fb08ad
Karan Bhalla <karanbhalla204@gmail.com>
2024-04-12 03:22:51 -0500

    Cleaned up employee view a bit

--- frontend/src/app/(employee)/employee/[category]/menuItems.js
+++ frontend/src/app/(employee)/employee/[category]/menuItems.js
@@ -5,37 +5,45 @@ import { useEffect, useState, useContext } from "react";
 import { TransactionContext, TransactionProvider, useTransaction } from "@/components/TransactionContext";
 
 function TransactionPanel() {
-    const {transactions, updateTransaction, clearTransaction, submitTransaction} = useTransaction();
+    const { transactions, updateTransaction, clearTransaction, submitTransaction } = useTransaction();
     const [transactionsList, setTransactionsList] = useState(null);
     useEffect(() => {
         setTransactionsList(transactions)
-    },[transactions])
-    return ( 
-            <div className="max-w-sm border-2 border-black rounded overflow-hidden shadow-lg mr-5 flex flex-col justify-between items-center">
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">Current Sale</div>
-              </div>
-              <div className="flex flex-col justify-evenly items-center">
-                    {transactionsList ? transactionsList.map((item, index) => {
-                            return <div key={index} className="items-center">{item.itemname} x{item.quantity} ${item.price}</div>
-                        }) : <div className="flex flex-col items-center">No items in current transaction!</div>}
-                    {/*{transactionsList ? <li>{transactionsList.length}</li> : <li>No items in current transaction!</li>}*/}
-              </div>
-              <div className="px-6 pt-4 pb-2 flex flex-col items-center">
-                <h1>Price: {
-                    transactionsList ? "$" + transactionsList.reduce((total, currentItem) => {return total + currentItem.price*currentItem.quantity}, 0).toFixed(2) : "$0.00"
-                }</h1>
-              </div>
+    }, [transactions])
+    return (
+        // flex grow flex-col justify-between items-center
+        <div className="flex flex-col grow justify-between border-2 border-black rounded overflow-hidden shadow-lg mx-5 mb-5 mt-2 p-5">
+            <div className="text-2xl font-bold text-center mt-2 mb-3">Current Sale</div>
 
-              <div className="px-6 pt-4 pb-2 flex flex-col items-center">
-                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={clearTransaction}>
-                    Clear Transaction
-                </button>
-                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={submitTransaction}>
-                    Charge
-                </button>
-              </div>
+            <div className="flex flex-col justify-evenly items-center">
+                {transactionsList ? transactionsList.map((item, index) => {
+                    return <div key={index} className="items-center">{item.itemname} x{item.quantity} ${item.price}</div>
+                }) : <div className="flex flex-col items-center">No items in current transaction!</div>}
+                {/*{transactionsList ? <li>{transactionsList.length}</li> : <li>No items in current transaction!</li>}*/}
             </div>
+            <div className="px-6 pt-4 pb-2 flex flex-col items-center">
+                <h1>Price: {
+                    transactionsList ? "$" + transactionsList.reduce((total, currentItem) => { return total + currentItem.price * currentItem.quantity }, 0).toFixed(2) : "$0.00"
+                }</h1>
+            </div>
+
+
+            <div className="flex flex-row justify-between">
+                <div>
+                    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={clearTransaction}>
+                        Clear Transaction
+                    </button>
+                </div>
+
+                <div>
+                    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={submitTransaction}>
+                        Charge
+                    </button>
+                </div>
+
+
+            </div>
+        </div>
     )
 }
 
@@ -48,28 +56,24 @@ function MenuItem(props) {
                 if (props.item.menuid == item.id) {
                     quantity = item.quantity + 1
                 }
-            }); 
+            });
         }
         if (quantity == 0) {
             quantity += 1
         }
-        updateTransaction({"id": props.item.menuid, "itemname": props.item.itemname, "price": props.item.price, "quantity": quantity})
+        updateTransaction({ "id": props.item.menuid, "itemname": props.item.itemname, "price": props.item.price, "quantity": quantity })
     }
 
     return (
-        <div className="relative bg-white rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out aspect-square" onClick={sendToTransaction}>
-            <img
-                alt={props.item.itemname}
-                className="object-cover w-full h-full rounded-lg"
-            />
-            <div className="absolute inset-0 flex flex-col justify-end p-4">
-                <h5 className="text-xl font-bold text-gray-900 text-center">{props.item.itemname}</h5>
+        <div className="flex justify-center px-10 py-14 items-center bg-white border border-gray rounded-lg shadow-md hover:shadow-xl transition duration-300" onClick={sendToTransaction}>
+            <div className="text-xl font-semibold text-gray-900 text-center">
+                {props.item.itemname}
             </div>
         </div>
     )
 }
 
-export function MenuItemList({categoryNum, categoryName}) {
+export function MenuItemList({ categoryNum, categoryName }) {
     const [itemType, setItemType] = useState([]);
 
     useEffect(() => {
@@ -83,20 +87,23 @@ export function MenuItemList({categoryNum, categoryName}) {
         fetchMenuItems();
     }, []);
 
-  return (
-    <div className="flex flex-row">
-        <div className="container mx-auto mr-5">
-            <h1 className="text-3xl font-bold text-center mb-8">{categoryName}</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {itemType.map((item, index) => (
-                    <MenuItem key = {index}
-                        item={item}
-                    />
-                ))}
+    return (
+
+        <div className="flex flex-row h-[75vh]">
+            <div className="container max-w-[66%] p-5">
+                <h1 className="text-3xl font-bold text-center mb-8">{categoryName}</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {itemType.map((item, index) => (
+                        <MenuItem key={index}
+                            item={item}
+                        />
+                    ))}
+                </div>
             </div>
+            
+            <TransactionPanel />
+
         </div>
-        <TransactionPanel/>
-    </div>
-  );
+    );
 }
 
