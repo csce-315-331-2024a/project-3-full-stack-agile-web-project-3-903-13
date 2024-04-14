5d74f44f068969ade45d2d685ee021109a35a734
Karan Bhalla <karanbhalla204@gmail.com>
2024-04-13 23:37:34 -0500

    Removed imports that weren't required

--- frontend/src/app/(employee)/employee/[category]/page.js
+++ frontend/src/app/(employee)/employee/[category]/page.js
@@ -1,9 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import {MenuItemList} from "./menuItems";
-import { useSearchParams } from 'next/navigation'
 
 
 const categories = [
@@ -21,11 +19,6 @@ const categories = [
 export default function EmployeePOSPage({params}) {
     const index = categories.indexOf(params.category);
     const name = categories[index].slice(0, 1).toUpperCase() + categories[index].slice(1)
-
-    const searchParams = useSearchParams()
-    console.log(searchParams.get('id')) // Logs "search"
-
-
     return (
         <main className="min-h-screen bg-cream py-1">
             <MenuItemList
