ce5856349eb996fa898d256b6959633dd3fb78c0
sukelv0802 <sukelv0802@gmail.com>
2024-04-25 17:54:09 -0500

    PaymentModal frontend remake

--- frontend/src/components/GoogleTranslate.js
+++ frontend/src/components/GoogleTranslate.js
@@ -5,7 +5,7 @@ const GoogleTranslateWidget = () => {
         <div className='fixed left-0 bottom-0 z-[1000] bg-white border-gray-400 border-2 rounded-xl px-2'>
             <div id="google_translate_element"></div>
             <Script
-                id="translate1"
+                id="translate1" 
                 src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
                 strategy="afterInteractive"
             />
