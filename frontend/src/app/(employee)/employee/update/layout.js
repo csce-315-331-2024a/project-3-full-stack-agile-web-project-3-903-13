f63fe72f5a8efc35eeeebb1ccb3fc638106cf48a
Aditya Biradar <adityajb014@gmail.com>
2024-04-28 13:29:20 -0500

    adding aria tags in manager page

--- frontend/src/app/(employee)/employee/update/layout.js
+++ frontend/src/app/(employee)/employee/update/layout.js
@@ -1,31 +1,33 @@
+import React from 'react';
 import EmployeeNavbar from "@/components/EmployeeNavbar";
-import "../../../globals.css";
 import { TransactionProvider } from "@/components/transactions/TransactionContext";
 import { Suspense } from "react";
 
 export default function CategoryLayout({ children }) {
   return (
     <TransactionProvider>
-    <>
-    <EmployeeNavbar
-        links={[
-          { name: "Inventory", route: "/employee/manager/inventory" },
-          { name: "Menu Items", route: "/employee/manager/menu-items" },
-          { name: "Inventory Usage", route: "/employee/manager/inventory-usage" },
-          { name: "Excess Report", route: "/employee/manager/excess-report" },
-          { name: "Sales Report", route: "/employee/manager/sales-report" },
-          { name: "Restock Report", route: "/employee/manager/restock-report" },
-          { name: "What Sells Duo", route: "/employee/manager/what-sells-together" },
-          { name: "Order Management", route: "/employee/manager/order-management" },
-          { name: "Kitchen", route: "/employee/manager/kitchen" }
-
-        ]}
-      ></EmployeeNavbar>
+      <>
+        <EmployeeNavbar
+          links={[
+            { name: "Inventory", route: "/employee/manager/inventory" },
+            { name: "Menu Items", route: "/employee/manager/menu-items" },
+            { name: "Inventory Usage", route: "/employee/manager/inventory-usage" },
+            { name: "Excess Report", route: "/employee/manager/excess-report" },
+            { name: "Sales Report", route: "/employee/manager/sales-report" },
+            { name: "Restock Report", route: "/employee/manager/restock-report" },
+            { name: "What Sells Duo", route: "/employee/manager/what-sells-together" },
+            { name: "Order Management", route: "/employee/manager/order-management" },
+            { name: "Kitchen", route: "/employee/manager/kitchen" }
+          ]}
+          aria-label="Employee Navigation"
+        />
       
-	  <Suspense>
-      	<main className="flex-1">{children}</main>
-      </Suspense>
-    </>
+        <Suspense fallback={<div>Loading...</div>}>
+          <main className="flex-1" aria-labelledby="category-layout-main">
+            {children}
+          </main>
+        </Suspense>
+      </>
     </TransactionProvider>
   );
 }
