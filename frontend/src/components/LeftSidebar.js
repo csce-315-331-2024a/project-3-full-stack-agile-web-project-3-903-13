b364deb99507cb1cc6dc7476e8f3c11dfdf5326b
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-04-28 14:37:41 -0500

    animations and selections

--- frontend/src/components/LeftSidebar.js
+++ frontend/src/components/LeftSidebar.js
@@ -4,30 +4,27 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
-export default function LeftSidebar({ links }) {
+export default function LeftSidebar({  }) {
   const pathname = usePathname();
+  const isActive = (href) => pathname === href;
+  const links = [
+    { href: "/employee/burgers", title: "Place an Order", imgSrc: "/shop.svg", imgAlt: "Point of Sale" },
+    { href: "/employee/manager/inventory", title: "Manager Features", imgSrc: "/user.svg", imgAlt: "Manager" },
+    { href: "/employee/manager/users", title: "User Management", imgSrc: "/users.svg", imgAlt: "Users" },
+    { href: "/", title: "Return to Customer View", imgSrc: "/home.svg", imgAlt: "Home" },
+    { href: "/user", title: "Sign Out", imgSrc: "/signout.svg", imgAlt: "Sign Out" }
+  ];
+
   return (
-    <>
     <nav className="w-[5rem] h-screen bg-white border-r-2 flex items-center flex-col">
       <Image src={"/revs.png"} alt="Rev's Grill Logo" width={0} height={0} sizes="100vw" className="w-full"></Image>
-      <div className="flex flex-col items-center gap-8 pt-8">
-      <Link href="/">
-        <Image alt = "Home" src={"/home.svg"} width={24} height={24}></Image>
-      </Link>
-      <Link title="Place an Order" href="/employee/burgers">
-        <Image alt = "Employee Point of Sale View" src={"/shop.svg"} width={24} height={24}></Image>
-      </Link>
-      <Link title="Manager Features" href="/employee/manager/inventory">
-        <Image alt = "Manager Homepage" src={"/user.svg"} width={24} height={24}></Image>
-      </Link>
-      <Link title="User Management" href="/employee/manager/users">
-        <Image alt = "User Management" src={"/users.svg"} width={24} height={24}></Image>
-      </Link>
-      <Link title="Sign Out" href="/user">
-        <Image alt = "Sign Out" src={"/signout.svg"} width={24} height={24}></Image>
-      </Link>
+      <div className="flex flex-col items-center pt-5">
+      {links.map(link => (
+        <Link className={`${isActive(link.href) ? "left-sidebar-link-active" : "left-sidebar-link"}`} key={link.href} href={link.href} title={link.title}>
+            <Image src={link.imgSrc} alt={link.imgAlt} width={24} height={24} />
+        </Link>
+      ))}
       </div>
     </nav>
-    </>
   );
 }
