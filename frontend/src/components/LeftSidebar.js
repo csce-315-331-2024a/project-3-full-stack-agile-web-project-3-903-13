f4928e7bc1f0202e93c0266e0af4a1292ee14667
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-04-28 14:01:59 -0500

    update leftsidebar with more images

--- frontend/src/components/LeftSidebar.js
+++ frontend/src/components/LeftSidebar.js
@@ -7,22 +7,27 @@ import { usePathname } from "next/navigation";
 export default function LeftSidebar({ links }) {
   const pathname = usePathname();
   return (
-    <nav className="w-[5rem] h-screen bg-white border-r-2 flex flex-col items-center gap-8 py-6">
+    <>
+    <nav className="w-[5rem] h-screen bg-white border-r-2 flex items-center flex-col">
+      <Image src={"/revs.png"} alt="Rev's Grill Logo" width={0} height={0} sizes="100vw" className="w-full"></Image>
+      <div className="flex flex-col items-center gap-8 pt-8">
       <Link href="/">
-        <Image alt = "rev logo" src={"/revs.png"} width={60} height={60}></Image>
+        <Image alt = "Home" src={"/home.svg"} width={24} height={24}></Image>
       </Link>
       <Link title="Place an Order" href="/employee/burgers">
-        <Image alt = "employee pos view" src={"/home.svg"} width={24} height={24}></Image>
+        <Image alt = "Employee Point of Sale View" src={"/shop.svg"} width={24} height={24}></Image>
       </Link>
       <Link title="Manager Features" href="/employee/manager/inventory">
-        <Image alt = "manager home" src={"/user.svg"} width={24} height={24}></Image>
+        <Image alt = "Manager Homepage" src={"/user.svg"} width={24} height={24}></Image>
       </Link>
       <Link title="User Management" href="/employee/manager/users">
-        <Image alt = "users" src={"/users.svg"} width={24} height={24}></Image>
+        <Image alt = "User Management" src={"/users.svg"} width={24} height={24}></Image>
       </Link>
       <Link title="Sign Out" href="/user">
-        <Image alt = "sign out" src={"/signout.svg"} width={24} height={24}></Image>
+        <Image alt = "Sign Out" src={"/signout.svg"} width={24} height={24}></Image>
       </Link>
+      </div>
     </nav>
+    </>
   );
 }
