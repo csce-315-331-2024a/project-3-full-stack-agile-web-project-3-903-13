1b793b4d723bb4159777665f8d49b2d47d956b40
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-04-29 12:11:27 -0500

    update weather to update live

--- frontend/src/components/WeatherAPI.js
+++ frontend/src/components/WeatherAPI.js
@@ -6,15 +6,29 @@ const WeatherWidget = ( {onWeatherLoaded} ) => {
     const [weatherInfo, setWeatherInfo] = useState(null);
     const [weatherIconUrl, setWeatherIconUrl] = useState('');
     const [currentTime, setCurrentTime] = useState(new Date());
+    const [location, setLocation] = useState(null);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentTime(new Date());
         }, 1000); // Update every second
-
-        // Clear interval on component unmount
         return () => clearInterval(interval);
     }, []);
+
+    useEffect(() => {
+        if (location) {
+            fetchWeatherData(location.latitude, location.longitude);
+        }
+    }, [location]);
+
+    useEffect(() => {
+        const weatherUpdateInterval = setInterval(() => {
+            if (location) {
+                fetchWeatherData(location.latitude, location.longitude);
+            }
+        }, 10000); // Update every 10 seconds
+        return () => clearInterval(weatherUpdateInterval);
+    }, [location]);
 
     // Format the date to display day of the week and date
     const formatDate = (date) => {
@@ -27,52 +41,43 @@ const WeatherWidget = ( {onWeatherLoaded} ) => {
         return date.toLocaleTimeString('en-US', { hour12: true, hour: 'numeric', minute: '2-digit' });
     };
 
+    const fetchWeatherData = async (latitude, longitude) => {
+        try {
+            const apiKey = '4a4358610dfaa3b0db4e33b4313a33f2';
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+            const data = response.data;
+            const place = data.name;
+            const temperature = data.main.temp;
+            const temperatureF = ((temperature - 273.15) * 9 / 5 + 32).toFixed(0);
+            const weatherDescription = data.weather[0].main;
+            const weatherIcon = data.weather[0].icon;
+
+            setWeatherInfo({
+                place,
+                temperatureF,
+                weatherDescription
+            });
+            setWeatherIconUrl(`https://openweathermap.org/img/wn/${weatherIcon}@4x.png`);
+            onWeatherLoaded(parseFloat(temperatureF));
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    };
+
     useEffect(() => {
-        // Function to fetch weather data based on user's location
-        const fetchWeatherData = async (latitude, longitude) => {
-            try {
-                const apiKey = '4a4358610dfaa3b0db4e33b4313a33f2';
-                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
-                const data = response.data;
-
-                // Extract relevant weather information
-                const place = data.name;
-                const temperature = data.main.temp;
-                const temperatureF = ((temperature - 273.15) * (9 / 5) + 32).toFixed(0);
-                const weatherDescription = data.weather[0].main;
-                const weatherIcon = data.weather[0].icon;
-
-                // Update weatherInfo state with weather data
-                setWeatherInfo({
-                    place, 
-                    temperatureF,
-                    weatherDescription
-                });
-                setWeatherIconUrl(`https://openweathermap.org/img/wn/${weatherIcon}@4x.png`);
-                onWeatherLoaded(parseFloat(temperatureF));
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-            }
-        };
-
-        // Function to get user's current location
-        const getLocation = () => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(position => {
-                    const latitude = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-                    fetchWeatherData(latitude, longitude);
-                }, error => {
-                    console.error('Error getting location:', error);
-                });
-            } else {
-                console.error('Geolocation is not supported by this browser.');
-            }
-        };
-
-        // Fetch weather data when component mounts
-        getLocation();
-    }, [onWeatherLoaded]);
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(position => {
+                const { latitude, longitude } = position.coords;
+                if (!location || latitude !== location.latitude || longitude !== location.longitude) {
+                    setLocation({ latitude, longitude });
+                }
+            }, error => {
+                console.error('Error getting location:', error);
+            });
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    }, []);
 
     return (
         <div className="flex items-center w-full justify-center pb-8" role="region" aria-label="Weather Information">
