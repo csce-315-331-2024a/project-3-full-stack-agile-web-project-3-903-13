9df421894a239491977606c5a7eff63167602e01
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-04-28 22:54:44 -0500

    remove a space

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const CustomerFooter = () => {
     return (
       <footer className="bg-[#800000] text-white p-8 flex flex-col justify-between">
         <div className="flex flex-row justify-between flex-wrap">
-          <div className="flex flex-col p-2 min-w-[150px]"aria-hidden="true">
+          <div className="flex flex-col p-2 min-w-[150px]" aria-hidden="true">
           </div>
           <div className="flex flex-col p-2 min-w-[150px] items-center text-lg">
             <h1 className="mb-4 font-bold">Located in: Memorial Student Center (MSC)</h1>
