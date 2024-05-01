55e2dedb0a1d6538592448733dd9232526aa73e0
Karan Bhalla <karanbhalla204@gmail.com>
2024-05-01 17:45:27 -0500

    Added documentation for /specific endpoint in menuitems route

--- api/routes/menuItems.js
+++ api/routes/menuItems.js
@@ -5,6 +5,29 @@ const express = require('express')
 const router = express.Router()
 const menuItemsController = require('../services/menuitems')
 
+router.post("/", menuItemsController.addMenuItem);
+router.get("/", menuItemsController.retrieveMenuItems);
+router.get("/getIngreds", menuItemsController.retrieveMenuItemIngredients);
+router.get("/seasonal", menuItemsController.retrieveSeasonalInfo);
+router.get("/specific", menuItemsController.getDetails)
+
+// UPDATE various attributes
+router.patch("/updatePrice", menuItemsController.updateMenuItemPrice);
+router.patch("/updateCat", menuItemsController.updateMenuItemCat);
+router.patch("/updateIngred", menuItemsController.updateMenuItemIngred);
+router.patch("/updateDesc", menuItemsController.updateMenuItemDescription);
+router.patch("/updateCal", menuItemsController.updateMenuItemCalories);
+router.patch("/updateDiet", menuItemsController.updateMenuItemDiet);
+router.patch("/updateAller", menuItemsController.updateMenuItemAllergy);
+
+// DELETE a menu item
+router.delete("/", menuItemsController.removeMenuItem);
+
+
+module.exports = router
+// vim: tabstop=3
+
+
 /**
  * @swagger
  * tags:
@@ -35,10 +58,53 @@ const menuItemsController = require('../services/menuitems')
  *	  get:
  *	    summary: Get list of ingredients of a given menu item
  *	    tags: [Menu Items]
- *	/menuitems/specific:
- *	  get:
- *	    summary: Gets the details of a menu item given its name
- *	    tags: [Menu Items]
+ * /menuitems/specific:
+ *   get:
+ *     summary: Gets the details of a menu item given its name
+ *     tags: [Menu Items]
+ *     parameters:
+ *       - name: name
+ *         in: query
+ *         description: The name of the menu item to retrieve details for
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Details of the menu item retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   menuid:
+ *                     type: integer
+ *                     description: The ID of the menu item
+ *                   itemname:
+ *                     type: string
+ *                     description: The name of the menu item
+ *                   price:
+ *                     type: number
+ *                     description: The price of the menu item
+ *                   category:
+ *                     type: integer
+ *                     description: The category ID of the menu item
+ *                   description:
+ *                     type: string
+ *                     description: The description of the menu item
+ *                   calories:
+ *                     type: integer
+ *                     description: The calorie count of the menu item
+ *                   specialdiet:
+ *                     type: integer
+ *                     description: The special diet ID of the menu item
+ *                   allergy:
+ *                     type: integer
+ *                     description: The allergy ID of the menu item
+ *       500:
+ *         description: Internal Server Error
  *	/menuitems/updatePrice:
  *	  patch:
  *	    summary: Get list of ingredients of a given menu item
@@ -68,26 +134,3 @@ const menuItemsController = require('../services/menuitems')
  *	    summary: Get list of ingredients of a given menu item
  *	    tags: [Menu Items]
  */
-
-
-router.post("/", menuItemsController.addMenuItem);
-router.get("/", menuItemsController.retrieveMenuItems);
-router.get("/getIngreds", menuItemsController.retrieveMenuItemIngredients);
-router.get("/seasonal", menuItemsController.retrieveSeasonalInfo);
-router.get("/specific", menuItemsController.getDetails)
-
-// UPDATE various attributes
-router.patch("/updatePrice", menuItemsController.updateMenuItemPrice);
-router.patch("/updateCat", menuItemsController.updateMenuItemCat);
-router.patch("/updateIngred", menuItemsController.updateMenuItemIngred);
-router.patch("/updateDesc", menuItemsController.updateMenuItemDescription);
-router.patch("/updateCal", menuItemsController.updateMenuItemCalories);
-router.patch("/updateDiet", menuItemsController.updateMenuItemDiet);
-router.patch("/updateAller", menuItemsController.updateMenuItemAllergy);
-
-// DELETE a menu item
-router.delete("/", menuItemsController.removeMenuItem);
-
-
-module.exports = router
-// vim: tabstop=3
