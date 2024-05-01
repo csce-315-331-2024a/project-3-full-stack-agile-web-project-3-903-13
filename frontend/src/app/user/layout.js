ff7e1922997bc94b7310590e1ab0eb88f966a51c
sukelv0802 <sukelv0802@gmail.com>
2024-04-30 22:25:19 -0500

    User management documentation done

--- frontend/src/app/user/layout.js
+++ frontend/src/app/user/layout.js
@@ -1,3 +1,8 @@
+/**
+ * @module UserManagement/Layout
+ * @description Root layout component that wraps children with global styles and appropriate HTML structure.
+ */
+
 import "../globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -7,6 +12,13 @@ export const metadata = {
   description: 'User Management',
 };
 
+/**
+ * Provides the root HTML structure for user management-related pages, encapsulating any child components passed to it.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {React.ReactNode} props.children - Child components to be rendered within the layout.
+ * @returns {React.Component} The root layout component with the specified children.
+ */
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
